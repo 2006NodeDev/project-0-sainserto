a1b96d6dff8114b4d16f14966714d0943183d968
Arlette Inserto <you@example.com>
2020-06-22 20:30:00 -0700

    created mock users and reimbursements

--- reimburzone/src/index.ts
+++ reimburzone/src/index.ts
@@ -1,10 +1,109 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import { User } from './models/User' 
+import { Reimbursement } from './models/Reimbursement'
 const app = express()
 
-app.use('/', (req, res) => {
-    res.send('Hello World')
+// app.use('/', (req, res) => {
+//     res.send('Hello World')
+// })
+app.use(express.json())
+
+app.get('/users', (req:Request, res:Response)=>{
+    res.json(users)
 })
 
+app.get('/reimbursements', (req:Request, res:Response)=>{
+    res.json(reimbursements)
+})
+
+app.post('/users', (req:Request, res:Response)=>{
+    console.log(req.body);
+    let {userId, 
+        username, 
+        password, 
+        firstName, 
+        lastName, 
+        email, 
+        role    
+    } = req.body
+
+        if(userId && username && password && firstName && lastName && email && role){
+            users.push({userId, username, password, firstName, lastName, email, role})
+            res.sendStatus(201)
+        }else{
+            res.status(400).send("Please Fill Out All Fields")    
+        }
+
+    // res.sendStatus(501);
+})
 app.listen(2006, () =>{
     console.log('Listening on port 2006');
 })
+
+let users:User[] = [
+    {
+        userId:1,
+        username:'sainserto',
+        password:'password',
+        firstName:'Arlette',
+        lastName:'Inserto',
+        email:'sai@google.com',
+        role:{
+            roleId:1,
+            role:'admin'
+        }
+    },
+    {
+        userId:2,
+        username:'bryle',
+        password:'password',
+        firstName:'Bryle',
+        lastName:'Peralta',
+        email:'bryle@google.com',
+        role:{
+            roleId:2,
+            role:'finance-manager'
+        }
+    },
+    {
+        userId:3,
+        username:'kyeo',
+        password:'password',
+        firstName:'Kyeo',
+        lastName:'Leo',
+        email:'kyeo@google.com',
+        role:{
+            roleId:3,
+            role:'member'
+        }
+    }
+]
+
+let reimbursements:Reimbursement[] = [
+    {
+        reimbursementId:1,
+        amount:333,
+        author:3,
+        dateSubmitted:2019,
+        dateResolved:2020,
+        description:'housing',
+        resolver:2,
+        status:{
+            statusId:2,
+            status:'resolved'
+        },
+        type:{
+            typeId:2,
+            type:'cash'
+        }
+    }
+]
+// reimbursementId: number
+//     author: number
+//     amount: number
+//     dateSubmitted: number
+//     dateResolved: number
+//     description: string
+//     resolver: number
+//     status: ReimbursementStatus
+//     type: ReimbursementType
---
+++ reimburzone/src/routers/user-router.ts
@@ -0,0 +1,5 @@
+// import express, { Request, Response, NextFunction } from 'express'
+
+
+// export const userRouter = express.Router()
+// userRouter.use(authentication)
